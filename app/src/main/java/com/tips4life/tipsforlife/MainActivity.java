86f21c6f09c531b3c7dae5b0071c62d42bf67a7a
Lachlan Eagling <lochie.eagling@gmail.com>
2017-09-10 15:33:11 +1000

    Implement random tip retrieval

--- app/src/main/java/com/tips4life/tipsforlife/MainActivity.java
+++ app/src/main/java/com/tips4life/tipsforlife/MainActivity.java
@@ -8,13 +8,18 @@ import android.widget.TextView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.Random;
+
+import static com.tips4life.tipsforlife.utils.FirebaseDatabaseHelper.getFirebaseDb;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseReference mDbRef = FirebaseDatabase.getInstance().getReference();
-    TextView mRandomTip;
+    private TextView mRandomTip;
+
+    // TODO: Implement an enum or something for categories.
+    public static final String CATEGORY_TECHNOLOGY = "Technology";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +28,22 @@ public class MainActivity extends AppCompatActivity {
 
         mRandomTip = (TextView) findViewById(R.id.txt_tip);
 
-        DatabaseReference techTip = mDbRef.child("tips").child("Technology").child("1");
-        techTip.addListenerForSingleValueEvent(new ValueEventListener() {
+        final DatabaseReference tips = getFirebaseDb().child("tips").child(CATEGORY_TECHNOLOGY);
+        tips.addListenerForSingleValueEvent(new ValueEventListener() {
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) // which obviously it does
                 {
-                    Log.i("Point", "Reached");
-                    mRandomTip.setText(dataSnapshot.getValue(String.class));
+                    // Generate random index bounded by total children in category.
+                    Long childrenCount = dataSnapshot.getChildrenCount();
+                    String index = randomIndex(childrenCount.intValue());
+                    String tip = dataSnapshot.child(index).getValue(String.class);
+                    mRandomTip.setText(tip);
+                    Log.d("Fetching tip: ", tip);
                 } else {
-                    String no_tips = "No Tips Found!";
-                    mRandomTip.setText(no_tips);
+                    mRandomTip.setText(R.string.no_tips_available);
+                    Log.e("Database empty: ", "No tips available!");
                 }
             }
 
@@ -42,4 +52,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private String randomIndex(int count){
+
+        // Generates a random number bounded by count of children in tips category.
+        // Need to cast and return as String to be passed as a valid path in Firebase.
+        Random rand = new Random();
+        Integer randomNumber = rand.nextInt(count + 1);
+
+        if(randomNumber == 0){
+            randomNumber = 1;
+        }
+
+        return randomNumber.toString();
+    }
+
 }
